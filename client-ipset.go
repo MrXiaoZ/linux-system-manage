6c51388d6847b7504667e8f1bdd1465c4734a1a5
MrXiaoZ <xiaogegg@gmail.com>
2015-03-03 15:16:29 +0800

    Create client-ipset.go

---
+++ client-ipset.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/hprose/hprose-go/hprose"
+	"flag"
+	"fmt"
+)
+
+type IpSet struct {
+    
+	IpSet func(string,string,string)(string,string,string)
+
+}
+
+
+func main() {
+
+//	act := flag.String("act", "", "please give me the command")
+	ip := flag.String("ip","","Please enter the ip address!")
+	mask := flag.String("mask","","Please enter the netmask!")
+	gateway := flag.String("gw","","Please enter the gateway!")
+    
+	flag.Parse()
+
+	client := hprose.NewClient("http://172.16.1.231:8080/")
+
+	var ipset *IpSet
+    client.UseService(&ipset)
+	fmt.Println(ipset.IpSet("1.1.1.1","24","1.1.1.254"))
+	fmt.Println(ipset.IpSet(*ip,*mask,*gateway))
+}
