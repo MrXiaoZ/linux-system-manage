b0102ffc4ed46929b101ec91d3313c439b53aaaf
MrXiaoZ <xiaogegg@gmail.com>
2015-03-04 13:50:12 +0800

    Update client-system.go

--- client-system.go
+++ client-system.go
@@ -17,14 +17,14 @@ type Reboot struct {
 func main() {
 
 	act := flag.String("act", "", "please give me the command")
-    flag.Parse()
+    	flag.Parse()
 
 	client := hprose.NewClient("http://172.16.1.231:8080/")
 
 	var shutdown *Shutdown
-    client.UseService(&shutdown)
-    var reboot *Reboot
-    client.UseService(&reboot)
+    	client.UseService(&shutdown)
+    	var reboot *Reboot
+    	client.UseService(&reboot)
 
     if *act != "" {
         switch *act {
